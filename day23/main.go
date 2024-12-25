c30ec06496aadeab08934a3010dfd1dc08c9b3e5
Noémi Salaün <noemi.salaun@gmail.com>
2024-12-25 22:09:39 +0100

    day 23

--- day23/main.go
+++ day23/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"sort"
 	"strings"
 )
+
+var theGraph map[string][]string
+var theCouples map[couple]struct{}
 
 type triplet struct {
 	c1 string
@@ -42,47 +46,119 @@ func main() {
 		return
 	}
 
-	var triplets = make(map[triplet]struct{})
+	theGraph = in.links
+	theCouples = in.couples
 
-	for c1, c1Links := range in.links {
-		if len(c1Links) < 2 {
-			continue
-		}
+	var pcs []string
+	for pc := range theGraph {
+		pcs = append(pcs, pc)
+	}
 
-		for _, c2 := range c1Links {
-			c2Links := in.links[c2]
-			if len(c2Links) < 2 {
-				continue
-			}
+	R := []string{}
+	P := pcs
+	X := []string{}
+	result := make(map[string]struct{})
 
-			inter := intersect(c1Links, c2, in.couples)
-			for _, c3 := range inter {
-				triplets[newTriplet(c1, c2, c3)] = struct{}{}
-			}
+	bronKerbosch(R, P, X, result)
+
+	var best string
+	var bestSize int
+	for r := range result {
+		if len(r) > bestSize {
+			best = r
+			bestSize = len(r)
 		}
 	}
 
-	count := 0
-	for k := range triplets {
-		if k.hasChief() {
-			count++
-		}
-	}
-
-	fmt.Println(count)
+	fmt.Println(best)
 }
 
-func intersect(c1Links []string, c2 string, coupleMap map[couple]struct{}) []string {
-	var result []string
+// Fonction pour convertir une liste triée en une clé unique
+func toKey(clique []string) string {
+	sort.Strings(clique)             // Trie les éléments de la clique
+	return strings.Join(clique, ",") // Crée une chaîne unique
+}
 
-	for _, c1Link := range c1Links {
-		cpl := newCouple(c1Link, c2)
-		if _, ok := coupleMap[cpl]; ok {
-			result = append(result, c1Link)
+// R: Une liste contenant les nœuds de la clique actuelle.
+// P: Une liste des candidats potentiels à ajouter à la clique.
+// X: Une liste des nœuds qui ne peuvent pas être dans la clique actuelle.
+func bronKerbosch(R []string, P []string, X []string, resultSet map[string]struct{}) {
+	if len(P) == 0 && len(X) == 0 {
+		key := toKey(R) // Convertit la clique triée en une clé unique
+		if _, ok := resultSet[key]; !ok {
+			resultSet[key] = struct{}{} // Stocke la clique unique dans le set
+		}
+		return
+	}
+	// Choisir un pivot
+	u := choosePivot(union(P, X))
+	neighbors := make(map[string]struct{})
+	for _, n := range theGraph[u] {
+		neighbors[n] = struct{}{}
+	}
+	var PNu []string
+	for _, p := range P {
+		if _, ok := neighbors[p]; !ok {
+			PNu = append(PNu, p)
 		}
 	}
 
+	// Explorer les nœuds de P qui ne sont pas voisins du pivot
+	for _, node := range PNu {
+		bronKerbosch(
+			append(R, node),
+			intersect(P, theGraph[node]),
+			intersect(X, theGraph[node]),
+			resultSet,
+		)
+		P = remove(P, node)
+		X = append(X, node)
+	}
+}
+
+func remove(slice []string, element string) []string {
+	var result []string
+	for _, v := range slice {
+		if v != element {
+			result = append(result, v)
+		}
+	}
 	return result
+}
+
+func choosePivot(P []string) string {
+	return P[rand.Intn(len(P))]
+}
+
+func intersect(list1 []string, list2 []string) []string {
+	set := make(map[string]struct{})
+	for _, v := range list2 {
+		set[v] = struct{}{}
+	}
+
+	var intersection []string
+	for _, v := range list1 {
+		if _, ok := set[v]; ok {
+			intersection = append(intersection, v)
+		}
+	}
+	return intersection
+}
+
+func union(a, b []string) []string {
+	set := make(map[string]bool)
+	for _, v := range a {
+		set[v] = true
+	}
+	for _, v := range b {
+		set[v] = true
+	}
+
+	var uni []string
+	for k := range set {
+		uni = append(uni, k)
+	}
+	return uni
 }
 
 type input struct {
